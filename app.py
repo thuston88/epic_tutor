6d5cd5396ce75c24ff313853f2ca77c0fea65b43
Tom Huston <thuston88@hotmail.com>
2016-01-14 20:08:57 -0600

    3 notes in mynote

--- app.py
+++ app.py
@@ -16,5 +16,21 @@ def homepage():
     except Exception, e:
         return str(e)
         
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    notes = [{ "name":"First Note Ever",
+        "author":"Tom Huston",
+        "content":"This text is coming from the content field"
+        },
+    { "name":"Finish this Blog",
+        "author":"Tom Huston",
+        "content":"Show the template control structures"
+        },
+    { "name":"Deploy this app to pythonanywhere",
+        "author":"Tom Huston",
+        "content":"When finished coding this app, deploy it to pythonanywhere"
+    }]       
+    return render_template("mynote.html", notes = notes)
+        
 if __name__ == "__main__":
     app.run()
